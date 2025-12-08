d314005e8d315d0a344c0c05128a0475361ff7c3
awk2824 <aryawira304@gmail.com>
2025-12-08 20:44:21 +0700

    Updated theme

--- frontend/BuildUpLandApp/Pages/Tentang/Tentang.xaml.cs
+++ frontend/BuildUpLandApp/Pages/Tentang/Tentang.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,6 +32,12 @@ namespace BuildUpLandApp.Pages.Tentang
             {
                 NavigationService.GoBack();
             }
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+            e.Handled = true;
         }
 
     }
